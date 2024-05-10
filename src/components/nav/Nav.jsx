71abdb561131ec358d0078b9b9ced45838c2c731
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-10 21:40:37 +0530

    Navbar modification based on Nav and router implementation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Nav.module.css";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function Nav() {
+  const navigate = useNavigate();
+  const [courses, setCourses] = useState(true)
+  const handleCourses = ()=>{
+    navigate("/courses")
+    if(courses){
+
+      setCourses(!courses);
+    }
+  }
+
+  const handleLogo = ()=>{
+    navigate("/")
+    if(!courses){
+
+      setCourses(!courses)
+    }
+  }
+
   return (
+    <>
     <nav>
       <div className={style.nav_container}>
         <div className={style.nav_title_wrapper}>
@@ -10,14 +30,17 @@ function Nav() {
             className={style.logo}
             src="https://files.codingninjas.in/pl-ninja-16706.svg"
             alt="logo"
+            onClick={handleLogo}
           />
           <h4>Coding Ninjas</h4>
         </div>
         <div className={style.nav_details}>
-          <button>Courses</button>
+          <button onClick={handleCourses}>{courses? "Courses" : "onCourses"}</button>
         </div>
       </div>
     </nav>
+    <Outlet/>
+    </>
   );
 }
 
